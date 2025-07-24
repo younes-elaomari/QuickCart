beaa7206f269d69b11b7879542cb8b24fe240ee7
Younes Elaomari <youneselaomari@gmail.com>
2025-07-24 10:53:24 -0700

    Initial Commit

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,15 @@ import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
@@ -12,6 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
       <html lang="en">
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
@@ -20,5 +30,6 @@ export default function RootLayout({ children }) {
           </AppContextProvider>
         </body>
       </html>
+    </ClerkProvider>
   );
 }
